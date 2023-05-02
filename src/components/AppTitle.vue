c946d37bb151c1e8cf67895b586651acec4b0ff2
gluke0 <milanesigianluca@gmail.com>
2023-05-02 17:19:49 +0200

    AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<!-- JS -->
+<script>
+export default {
+    data() {
+        return {
+            title: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+
+<!-- HTML -->
+<template>
+    <h1>{{ title }}</h1>
+</template>
+
+<!-- CSS -->
+<style scoped>
+h1 {
+    font-size: 4rem;
+    text-align: center;
+}
+</style>
